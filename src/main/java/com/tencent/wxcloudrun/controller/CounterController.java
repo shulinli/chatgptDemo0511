bc4795074b3b72e35329e78d92695de813699e87
shulin <805649973@qq.com>
2023-05-11 19:16:08 +0800

    0511 commit

--- src/main/java/com/tencent/wxcloudrun/controller/CounterController.java
+++ src/main/java/com/tencent/wxcloudrun/controller/CounterController.java
@@ -10,6 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.time.LocalDateTime;
