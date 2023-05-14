b7a1deff709f3b0415a58bf009c42681646ebb8d
shulin <805649973@qq.com>
2023-05-14 18:54:00 +0800

    0511 commit

--- src/main/java/com/tencent/wxcloudrun/controller/CounterController.java
+++ src/main/java/com/tencent/wxcloudrun/controller/CounterController.java
@@ -1,5 +1,10 @@
 package com.tencent.wxcloudrun.controller;
 
+import com.alibaba.fastjson.JSONObject;
+import okhttp3.MediaType;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import com.tencent.wxcloudrun.config.ApiResponse;
@@ -13,11 +18,9 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.IOException;
 import java.time.LocalDateTime;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Optional;
-import java.util.List;
+import java.util.*;
 
 /**
  * counter控制器
@@ -89,14 +92,68 @@ public class CounterController {
    */
   @PostMapping(value = "/api/count")
   @ResponseBody
-  Map<String, String> create(@RequestBody Map<String, String> request) {
+  Map<String, String> create(@RequestBody Map<String, String> request) throws IOException {
     logger.info("/api/count post request");
     Map<String, String> response = new HashMap<String, String>();
     response.put("ToUserName", request.get("FromUserName"));
     response.put("FromUserName", request.get("ToUserName"));
     response.put("CreateTime", "123456789");
     response.put("MsgType", request.get("MsgType"));
-    response.put("Content","![CDATA[你好]]");
+
+
+    System.setProperty("http.proxyHost", "127.0.0.1");
+    System.setProperty("http.proxyPort", "7890");
+
+    System.setProperty("https.proxyHost", "127.0.0.1");
+    System.setProperty("https.proxyPort", "7890");
+
+    OkHttpClient client = new OkHttpClient();
+
+    String url = "https://api.openai.com/v1/chat/completions";
+
+    String apiKey = "Bearer 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";
+
+    String message = "[{\"role\": \"user\", \"content\": \"Hello!\"}]";
+
+    ArrayList<Map<String, String>> list = new ArrayList<>();
+    Map<String, String> map = new HashMap<>();
+    map.put("role", "user");
+    map.put("content", "hello");
+    list.add(map);
+    Map<String, Object> map2 = new HashMap<>();
+    map2.put("model", "gpt-3.5-turbo");
+    map2.put("messages", list);
+    String str= new JSONObject(map2).toString();
+
+    Request req = new Request.Builder()
+
+            .url(url)
+
+            .addHeader("Authorization", apiKey)
+            .addHeader("Content-Type", "application/json")
+
+            .post(okhttp3.RequestBody.create(MediaType.parse("application/json"), str))
+
+            .build();
+
+    Response res = client.newCall(req).execute();
+
+    String responseBody = res.body().string();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    response.put("Content",responseBody);
     return response;
   }
 
